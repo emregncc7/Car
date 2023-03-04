2ba30e15d1525b33feb8e867f3380a4ad2d8e0fd
emre.genc <emre.genc@KDK-102-PC04-YZ>
2023-03-04 15:53:00 +0300

    Add project files.

---
+++ Car.DataAccess/Concrete/EntityFramework/ArabaDal.cs
@@ -0,0 +1,19 @@
+﻿using Car.Core.DataAccess.EntityFramework;
+using Car.DataAccess.Abstract;
+using Car.DataAccess.Concrete.EntityFramework.Context;
+using Car.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.DataAccess.Concrete.EntityFramework
+{
+    public class ArabaDal:RepositoryBase<Araba>,IArabaDal
+    {
+        public ArabaDal(CarContext context):base(context)
+        {
+        }
+    }
+}
---
+++ Car.Entities/Concrete/Araba.cs
@@ -0,0 +1,24 @@
+﻿using Car.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.Entities.Concrete
+{
+    public class Araba:IEntity
+    {
+        public Araba()
+        {
+            CreateDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+        public bool HasarDurumu { get; set; }
+        public int Yil { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
